ec6c1ec2ed2323776103dbf8e9e1b117252b7b83
anaPadurean <ana_mariapadurean@yahoom.com>
2025-06-28 11:31:27 +0300

    Update index.js to handle env variable

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./service-account.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || require('./service-account.json'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
